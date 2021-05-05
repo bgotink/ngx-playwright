15703c6842f764e4b8fe0ee7c2dd6bd632d6c83f
Bram Gotink <bram@gotink.me>
2021-05-05 23:52:50 +0200

    {fix} remove unsupported properties from the screen destructure

--- packages/jest/src/index.ts
+++ packages/jest/src/index.ts
@@ -19,6 +19,16 @@ export type PlaywrightScreen<T extends ComponentHarness> =
   | PlaywrightScreenWithOpenFunction<T>
   | PlaywrightScreenWithPath<T>;
 
+type ExtractablePropertyNamesOfScreen<T extends ComponentHarness> = {
+  [K in keyof T]: T[K] extends AsyncFactoryFn<unknown> ? K : never;
+}[keyof T];
+
 export type ExtractablePropertiesOfScreen<T extends ComponentHarness> = {
-  [K in keyof T]: T[K] extends AsyncFactoryFn<infer P> ? P : never;
+  // Once updated to angular 12 (typescript 4.2) replace the intermediary type with
+  // [K in ExtractablePropertyNamesOfScreen<T> as T[K] extends AsyncFactoryFn<unknown> ? K : never]
+  [K in ExtractablePropertyNamesOfScreen<T>]: T[K] extends AsyncFactoryFn<
+    infer P
+  >
+    ? P
+    : never;
 };
