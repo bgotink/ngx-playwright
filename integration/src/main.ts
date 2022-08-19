fa05b3c12f2207238b26775ec111f48f506be041
Bram Gotink <bram@gotink.me>
2022-08-20 00:12:49 +0200

    {chore} add tests in production mode

--- integration/src/main.ts
+++ integration/src/main.ts
@@ -1,11 +1,20 @@
-import {ApplicationRef} from '@angular/core';
+import {ApplicationRef, enableProdMode} from '@angular/core';
 import {enableDebugTools} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
+import {production} from './env/env';
+
+if (production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then(appModuleRef => {
-    enableDebugTools(appModuleRef.injector.get(ApplicationRef).components[0]!);
+    if (!production) {
+      enableDebugTools(
+        appModuleRef.injector.get(ApplicationRef).components[0]!,
+      );
+    }
   });
